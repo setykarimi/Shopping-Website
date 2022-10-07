75ea522ba27f61a413766168473c1dd3861371be
setykarimi <setykarimi80@gmail.com>
2022-10-07 13:17:51 +0330

    add Cart Provider

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.scss';
 import Layout from './Layout/Layout';
 import CartPage from './pages/CartPage';
 import HomePage from './pages/HomePage';
+import CartProvider from './Providers/CartProvider';
 
 function App() {
   return (
     <div className="App">
-     <Layout>
-        <Routes>
-          <Route path='/' element=<HomePage/> />
-          <Route path='/cart' element=<CartPage/> />
-        </Routes>
+      <Layout>
+        <CartProvider>
+          <Routes>
+            <Route path='/' element=<HomePage /> />
+            <Route path='/cart' element=<CartPage /> />
+          </Routes>
+        </CartProvider>
       </Layout>
     </div>
   );
