3466f5e44d7c2b9236b24320bd8f0dfe551feb78
setykarimi <setykarimi80@gmail.com>
2022-10-07 13:19:15 +0330

    add add button and addCart function

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,28 @@
-
+import * as data from '../data';
+import { useCartActions } from '../Providers/CartProvider';
 const HomePage = () => {
+    const dispatch = useCartActions() 
+    const AddProductHandler =(product) => {
+        dispatch({type: 'ADD_TO_CART', payload: product})
+    }
     return (
-        <h2>this is home page</h2>
+        <main>
+            <section className='products-list'>
+                {data.products.map((product) =>
+                    <section className='product-list__product' key={product.id}>
+                        <div>
+                            <img src={product.image} alt={product.name} />
+                        </div>
+                        <div>
+                            <p>{product.name}</p>
+                            <p>{product.price}</p>
+                        </div>
+                            <button onClick={() => AddProductHandler(product)}>Add to cart</button>
+                    </section>
+                )}
+            </section>
+        </main>
     );
 }
- 
+
 export default HomePage;
