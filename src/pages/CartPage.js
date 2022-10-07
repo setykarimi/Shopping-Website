4d5b41e6c2138e8e194961bcc92aabcf05ace76f
setykarimi <setykarimi80@gmail.com>
2022-10-07 13:18:24 +0330

    add Cart data to cart page

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,5 +1,21 @@
+import { useCart } from "../Providers/CartProvider";
+
 const CartPage = () => {
-    return ( <h2>Cart page</h2> );
+    const { cart } = useCart()
+    
+    if(!cart) {
+        return (
+        <main><h2>Cart is empty</h2></main>
+        )
+    }
+
+    return (
+        <h2>
+            {cart.map(item =>
+                <div key={item.id}><h3>{item.name}</h3></div>)
+                }
+        </h2>
+    );
 }
- 
+
 export default CartPage;
