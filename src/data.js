50888a72deb456bc401822c0a31d63dc1f219b5d
setykarimi <setykarimi80@gmail.com>
2022-10-07 13:17:36 +0330

    add id to products

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export const products = [
+    {
+        id: 1,
+        name: "WInter body",
+        description : [
+            {support: "گارانتی مادام العمر"},
+            {support: "ارسال رایگان"},
+            {support:  "اورجینال"},
+        ],
+        price: 120,
+        offPrice: 120,
+        discount: 0,
+        image: 'https://s4.uupload.ir/files/item1_soj.jpg'
+    },
+    {
+        id: 2,
+        name: "Adidas",
+        description : [
+            {support: "ارسال رایگان"},
+            {support:  "اورجینال"},
+        ],
+        price: 110,
+        offPrice: 110,
+        discount: 0,
+        image: 'https://s4.uupload.ir/files/item1_soj.jpg'
+    },
+    {
+        id: 3,
+        name: "Nike",
+        description : [
+            {support: "گرانتی مادام العمر"},
+            {support:  "اورجینال"},
+        ],
+        price: 400,
+        offPrice: 390,
+        discount: 0,
+        image: 'https://s4.uupload.ir/files/item1_soj.jpg'
+    },
+]
