d3c786425e4d09591a805ee5dfc6d6c615d15f64
setykarimi <setykarimi80@gmail.com>
2022-10-09 22:27:12 +0330

    set navbar Logo in right side

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,12 +5,12 @@ const Navigation = () => {
     return (
         <header className="main-nav">
             <nav>
-            <h2>Shopping</h2>
+            
                 <ul>
                     <li><NavLink to='/'>Home</NavLink></li>
                     <li><NavLink to='/cart'>Cart</NavLink></li>
                 </ul>
-                
+                <h2>Shopping Center</h2>
             </nav>
         </header>
     );
