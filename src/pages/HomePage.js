f586d1297221cbc308c4bb2e9777b7867a46668b
setykarimi <setykarimi80@gmail.com>
2022-10-09 22:10:37 +0330

    add new classes for product List

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,23 +1,30 @@
 import * as data from '../data';
 import { useCartActions } from '../Providers/CartProvider';
+import './HomePage.scss'
 const HomePage = () => {
-    const dispatch = useCartActions() 
-    const AddProductHandler =(product) => {
-        dispatch({type: 'ADD_TO_CART', payload: product})
+    const dispatch = useCartActions()
+    const AddProductHandler = (product) => {
+        dispatch({ type: 'ADD_TO_CART', payload: product })
     }
     return (
-        <main>
+        <main className='container'>
             <section className='products-list'>
                 {data.products.map((product) =>
-                    <section className='product-list__product' key={product.id}>
+                    <section className='products-list__item' key={product.id}>
                         <div>
                             <img src={product.image} alt={product.name} />
                         </div>
-                        <div>
-                            <p>{product.name}</p>
-                            <p>{product.price}</p>
+                        <div className='products-list__item-name'>
+                            <span>{product.name}</span>
                         </div>
+                        <div className='products-list__item-details'>
+                            <div className='products-list__item-details-price'>
+                                <span>price</span>
+                                <b>$ {product.price} </b>
+                            </div>
                             <button onClick={() => AddProductHandler(product)}>Add to cart</button>
+                        </div>
+
                     </section>
                 )}
             </section>
