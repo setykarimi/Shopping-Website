0c46c312f7de7a13db55be38f70a18f2d8ad7b07
setykarimi <setykarimi80@gmail.com>
2022-10-09 22:11:38 +0330

    add empty title for empty cart

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -2,19 +2,33 @@ import { useCart } from "../Providers/CartProvider";
 
 const CartPage = () => {
     const { cart } = useCart()
-    
-    if(!cart) {
+
+    if (!cart) {
         return (
-        <main><h2>Cart is empty</h2></main>
+            <main><h2>Cart is empty</h2></main>
         )
     }
 
     return (
-        <h2>
-            {cart.map(item =>
-                <div key={item.id}><h3>{item.name}</h3></div>)
+        <main className="container">
+            <section>
+                {cart.map(item =>
+                    <div key={item.id} className="cartItem">
+                        <img src={item.image} alt={item.name}/>
+                        <h3>Name: {item.name}</h3>
+                        <h4>Price: {item.price * item.quantity}</h4>
+                        <div>
+                            <button>remove</button>
+                            <button>{item.quantity}</button>
+                            <button>Add</button>
+                        </div>
+                    </div>
+                  
+                    )
                 }
-        </h2>
+            </section>
+            <section>cart summary</section>
+        </main>
     );
 }
 
