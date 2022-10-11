d73af6918fa5cc31227b62d429c36fb6bfdc423a
setykarimi <setykarimi80@gmail.com>
2022-10-11 21:09:36 +0330

    add Decrement Handler, and add total price in updating state

--- src/Providers/cartReducer.js
+++ src/Providers/cartReducer.js
@@ -1,19 +1,44 @@
-export const cartReducer = (state,action) => {
+
+export const cartReducer = (state, action) => {
     switch (action.type) {
-        case 'ADD_TO_CART':
+        case 'ADD_TO_CART': {
             const updatedCart = [...state.cart]
-            const updatedItemIndex = updatedCart.findIndex((item) => item.id === action.payload.id)
-            if(updatedItemIndex < 0){
-                updatedCart.push({...action.payload, quantity: 1})
-            }else{
-                const updatedItem = {...updatedCart[updatedItemIndex]}
+            const updatedItemIndex = updatedCart.findIndex(
+                (item) => item.id === action.payload.id)
+
+            if (updatedItemIndex < 0) {
+                updatedCart.push({ ...action.payload, quantity: 1 })
+            } else {
+                const updatedItem = { ...updatedCart[updatedItemIndex] }
                 updatedItem.quantity++;
                 updatedCart[updatedItemIndex] = updatedItem;
             }
 
-            return {...state,cart: updatedCart}
-           
-    
+            return {
+                ...state,
+                cart: updatedCart,
+                total: state.total + action.payload.price
+            }
+        }
+        case 'REMOVE_PRODUCT': {
+            const updatedCart = [...state.cart];
+            const updatedItemIndex = updatedCart.findIndex(
+                (item) => item.id === action.payload.id);
+            const updatedItem = { ...updatedCart[updatedItemIndex] };
+            if (updatedItem.quantity === 1) {
+                const filteredCart = updatedCart.filter(item => item.id !== action.payload.id)
+                return { ...state, cart: filteredCart }
+            } else {
+                updatedItem.quantity--;
+                updatedCart[updatedItemIndex] = updatedItem;
+                return {
+                    ...state,
+                    cart: updatedCart,
+                    total: state.total + action.payload.price
+                }
+            }
+            return;
+        }
         default:
             return state
             break;
