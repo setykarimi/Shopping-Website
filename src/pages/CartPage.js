5b961889a121eb510a309c886ee83b67f9b2ca5e
setykarimi <setykarimi80@gmail.com>
2022-10-11 21:09:01 +0330

    add total price to CartPage

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,12 +1,21 @@
-import { useCart } from "../Providers/CartProvider";
+import { useCart, useCartActions } from "../Providers/CartProvider";
 
 const CartPage = () => {
-    const { cart } = useCart()
+    const { cart,total } = useCart()
+    const  dispatch  = useCartActions()
 
     if (!cart) {
         return (
             <main><h2>Cart is empty</h2></main>
         )
+    }
+
+    const IncrementHandler = (cartItem) => {
+        dispatch({type: "ADD_TO_CART", payload:cartItem})
+    }
+
+    const DecrementHandler = (cartItem) => {
+        dispatch({type: "REMOVE_PRODUCT", payload:cartItem})
     }
 
     return (
@@ -18,16 +27,19 @@ const CartPage = () => {
                         <h3>Name: {item.name}</h3>
                         <h4>Price: {item.price * item.quantity}</h4>
                         <div>
-                            <button>remove</button>
+                            <button onClick={() => DecrementHandler(item)}>remove</button>
                             <button>{item.quantity}</button>
-                            <button>Add</button>
+                            <button onClick={() => IncrementHandler(item)}>Add</button>
                         </div>
                     </div>
                   
                     )
                 }
             </section>
-            <section>cart summary</section>
+            <section className="cart-summary">
+                <h3>cart summary</h3>
+                <h5>{total}</h5>
+            </section>
         </main>
     );
 }
