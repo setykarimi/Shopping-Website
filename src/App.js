32e7a7bd87bb2f6514ccf82b74852bc6b5306697
setykarimi <setykarimi80@gmail.com>
2022-10-12 22:06:52 +0330

    Add react toastify contatienr and css

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import Layout from './Layout/Layout';
 import CartPage from './pages/CartPage';
 import HomePage from './pages/HomePage';
 import CartProvider from './Providers/CartProvider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
   return (
     <div className="App">
+    <ToastContainer />
       <Layout>
         <CartProvider>
           <Routes>
