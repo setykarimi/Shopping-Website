c1236bad3908067994661dae6ec08339d6144499
setykarimi <setykarimi80@gmail.com>
2022-10-15 22:11:19 +0330

    add navigation and edit submit Handler

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,40 +1,54 @@
 import Input from "../../common/Input";
 import { useFormik } from "formik";
 import * as Yup from 'yup';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { loginService } from "../../services/loginService";
+
 
 const initialValues = {
     email: "",
     password: "",
 }
 
-const onSubmit = (values) => {
-    console.log(values);
-}
 
 const validationSchema = Yup.object({
     email: Yup.string().email('Invalid email format').required('Email is required'),
     password: Yup.string().required('Passsword is required'),
 })
 
-const Login = () => {
+
+
+const LoginForm = () => {
+    const navigate = useNavigate();
+
+    const onSubmit = async (values) => {
+        try{
+            const {data} = await loginService(values);
+            navigate('/')
+        }catch(error){
+          toast.error(error.response.data.message)
+        }
+    }
+
     const formik = useFormik({
         initialValues,
         onSubmit,
         validationSchema,
         validateOnMount: true
     })
+
+
     return (
         <div className="form">
             <h3>Login Form</h3>
-            <form onSubmit={onSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <Input formik={formik} label="Email" name="email" type="email" />
                 <Input formik={formik} label="Password" name="password" type="password" />
                 <button type="submit" disabled={!formik.isValid} className="btn-orange">Login</button>
             </form>
-            <Link to="/signup" style={{marginTop: "1em", display: "block"}}>Dont have an account? <b className="orange">Signup</b></Link>
-        </div>
-    );
+            <Link to="/signup" style={{ marginTop: "1em", display: "block" }}>Dont have an account? <b className="orange">Signup</b></Link>
+        </div>);
 }
 
-export default Login;
+export default LoginForm;
