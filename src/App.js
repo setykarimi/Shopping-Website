e8f117c50a819c1b426bcc2c5e3bccf81ea6a3cb
setykarimi <setykarimi80@gmail.com>
2022-10-16 21:57:10 +0330

    add auth provider and add Profile page route

--- src/App.js
+++ src/App.js
@@ -9,23 +9,28 @@ import 'react-toastify/dist/ReactToastify.css';
 import CheckoutPage from './pages/CheckoutPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import AuthProvider from './Providers/AuthProvider';
+import ProfilePage from './pages/ProfilePage';
 
 
 function App() {
   return (
     <div className="App">
-    <ToastContainer />
-    <CartProvider>
-      <Layout>
-          <Routes>
-            <Route path='/' element=<HomePage /> />
-            <Route path='/cart' element=<CartPage /> />
-            <Route path='/checkout' element=<CheckoutPage /> />
-            <Route path='/login' element=<LoginPage /> />
-            <Route path='/signup' element=<SignupPage /> />
-          </Routes>
-      </Layout>
-      </CartProvider>
+      <AuthProvider>
+        <ToastContainer />
+        <CartProvider>
+          <Layout>
+            <Routes>
+              <Route path='/' element=<HomePage /> />
+              <Route path='/cart' element=<CartPage /> />
+              <Route path='/checkout' element=<CheckoutPage /> />
+              <Route path='/login' element=<LoginPage /> />
+              <Route path='/signup' element=<SignupPage /> />
+              <Route path='/profile' element=<ProfilePage /> />
+            </Routes>
+          </Layout>
+        </CartProvider>
+      </AuthProvider>
     </div>
   );
 }
