7f8735f018fe450910027122643737922d8de93a
setykarimi <setykarimi80@gmail.com>
2022-10-23 09:49:09 +0330

    add Data for profile Page

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,10 +1,16 @@
 import { useAuth } from "../Providers/AuthProvider";
 
 const ProfilePage = () => {
-    const userdata = useAuth()
-    return ( <div>
-        Profile
-    </div> );
+    const userData = useAuth();
+    console.log(userData);
+    return (
+        <div>
+           <p>{userData.name}</p>
+           <p>{userData.email}</p>
+           <p>{userData.phoneNumber}</p>
+           <p>{userData.isAdmin}</p>
+        </div>
+    );
 }
- 
+
 export default ProfilePage;
