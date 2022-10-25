4ea78592fa7607988ff2313c4d494fd68fb7c6a2
setykarimi <setykarimi80@gmail.com>
2022-10-25 13:27:25 +0330

    edit redirect navigate

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -37,7 +37,7 @@ const SignupForm = () => {
     const auth = useAuth()
 
     useEffect(() => {
-        if(auth) navigate(redirect)
+        if(auth) navigate(`/${redirect}`)
     },[redirect,auth])
 
 
