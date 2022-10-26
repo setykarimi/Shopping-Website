2c00a1fa1a5f7a0f78d33f7872660a6a6fa06dd1
setykarimi <setykarimi80@gmail.com>
2022-10-26 19:25:11 +0330

    add tailwind class instead of custom Classes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,23 +8,37 @@ const Navigation = () => {
     const userData = useAuth();
 
     return (
-        <header className="main-nav">
-            <nav>
-                <ul>
-                    <li><NavLink end to='/' className={(navData) => navData.isActive ? 'active-link' : "" }>Home</NavLink></li>
+        <header className="bg-blue-100 mb-10">
+            <nav className="container flex justify-between m-auto py-6 px-6 md:px-2">
+                <ul className="hidden md:flex space-x-4 md:flex-row items-center">
                     <li>
-                        <NavLink to='/cart' className={(navData) => navData.isActive ? 'active-link' : "" }>Cart
-                          <span className="cart-length">{cart.length}</span>
+                        <NavLink end to='/'
+                            className={(navData) => navData.isActive ?
+                                'transition ease-in-ou hover:no-underline font-black text-white hover:bg-blue-300 bg-blue-500 rounded-md px-2 py-1' :
+                                "transition ease-in-ou hover:no-underline hover:bg-blue-300 rounded-md hover:text-white px-2 py-1"}>
+                            Home
                         </NavLink>
                     </li>
                     <li>
-                    
-                        <NavLink to={userData ? "/profile" : "/signup"} className={(navData) => navData.isActive ? 'active-link' : "" }>
+                        <NavLink to='/cart'
+                            className={(navData) => navData.isActive ?
+                                'transition ease-in-ou hover:no-underline font-black text-white hover:bg-blue-300 bg-blue-500 rounded-md px-2 py-1 flex items-center' :
+                                "transition ease-in-ou hover:no-underline hover:bg-blue-300 rounded-md hover:text-white px-2 py-1 flex items-center"}>
+                            Cart
+                            <span className="bg-blue-400 ml-1 px-1 rounded-full text-white block w-5 h-5 flex items-center justify-center">{cart.length}</span>
+                        </NavLink>
+                    </li>
+                    <li>
+
+                        <NavLink to={userData ? "/profile" : "/signup"}
+                            className={(navData) => navData.isActive ?
+                                'transition ease-in-ou hover:no-underline font-black text-white hover:bg-blue-300 bg-blue-500 rounded-md px-2 py-1' :
+                                "transition ease-in-ou hover:no-underline hover:bg-blue-300 rounded-md hover:text-white px-2 py-1"}>
                             {userData ? "Profile" : "login/signup"}
                         </NavLink>
                     </li>
                 </ul>
-                <h2>Shopping Center</h2>
+                <h2 className="text-2xl font-black text-blue-500 tracking-widest">Shopping Center</h2>
             </nav>
         </header>
     );
