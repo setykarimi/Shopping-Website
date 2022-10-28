35b69240fdfa6fa64e991a4e0d98974aaa708f6e
setykarimi <setykarimi80@gmail.com>
2022-10-28 12:29:55 +0330

    add project routes to routes file

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import CartPage from "../pages/CartPage";
+import CheckoutPage from "../pages/CheckoutPage";
+import HomePage from "../pages/HomePage";
+import LoginPage from "../pages/LoginPage";
+import ProfilePage from "../pages/ProfilePage";
+import SignupPage from "../pages/SignupPage";
 
 const ProjectRoutes = () => {
-  
+    const routes = [
+        { path: '/', element: <HomePage /> },
+        { path: '/cart', element: <CartPage /> },
+        { path: '/checkout', element: <CheckoutPage /> },
+        { path: '/login', element: <LoginPage /> },
+        { path: '/signup', element: <SignupPage /> },
+        { path: '/profile', element: <ProfilePage /> },
+    ]
 
     return (
-      <></>
+        <Routes>
+            {routes.map((route) => <Route path={route.path} element={route.element} />)}
+        </Routes>
     )
 }
 
