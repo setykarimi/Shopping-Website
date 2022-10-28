a30ca867c80443d4c56e160c70c8e3e1b320b323
setykarimi <setykarimi80@gmail.com>
2022-10-28 12:30:26 +0330

    remove routes and page components and add projectRoutes component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.scss';
 import Layout from './Layout/Layout';
-import CartPage from './pages/CartPage';
-import HomePage from './pages/HomePage';
 import CartProvider from './Providers/CartProvider';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import CheckoutPage from './pages/CheckoutPage';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
 import AuthProvider from './Providers/AuthProvider';
-import ProfilePage from './pages/ProfilePage';
+import ProjectRoutes from './routes/routes';
 
 
 function App() {
@@ -20,14 +15,7 @@ function App() {
         <ToastContainer />
         <CartProvider>
           <Layout>
-            <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='/cart' element={<CartPage />} />
-              <Route path='/checkout' element={<CheckoutPage />} />
-              <Route path='/login' element={<LoginPage />} />
-              <Route path='/signup' element={<SignupPage />} />
-              <Route path='/profile' element={<ProfilePage />} />
-            </Routes>
+           <ProjectRoutes />
           </Layout>
         </CartProvider>
       </AuthProvider>
