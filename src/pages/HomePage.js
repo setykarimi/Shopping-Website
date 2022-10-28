ea4c66ee3994067a012a35bc528eb63097e922ae
setykarimi <setykarimi80@gmail.com>
2022-10-28 12:23:18 +0330

    add tailwind classes for styles and add Shopping icon from react-icons

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,9 +2,9 @@ import * as data from '../data';
 import { useCart, useCartActions } from '../Providers/CartProvider';
 import { checkInCart } from '../utils/checkInCart';
 import { toast } from 'react-toastify';
-import './HomePage.scss';
 import { useEffect, useState } from 'react';
 import http from '../services/httpService';
+import { FiShoppingCart } from 'react-icons/fi'
 
 
 const HomePage = () => {
@@ -29,23 +29,25 @@ const HomePage = () => {
         return (<p>loading ...</p>)
     }
     return (
-        <main className='container m-auto'>
-            <section className='products-list'>
+        <main className='md:container md:mx-auto px-5 md:px-0'>
+            <section className='grid grid-cols-1 gap-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-1'>
                 {products.map((productItem) =>
-                    <section className='products-list__item' key={productItem._id}>
+                    <section className='bg-white p-3 shadow-lg rounded-lg' key={productItem._id}>
                         <div>
-                            <img src={productItem.image} alt={productItem.name} />
+                            <img className='h-32 xl:h-40 rounded m-auto' src={productItem.image} alt={productItem.name} />
                         </div>
-                        <div className='products-list__item-name'>
+                        <div className='font-black text-left mt-4 text-xl pb-1 border-b border-slate-200'>
                             <span>{productItem.name}</span>
                         </div>
-                        <div className='products-list__item-details'>
-                            <div className='products-list__item-details-price'>
-                                <span>price</span>
-                                <b>$ {productItem.price} </b>
+                        <div className='flex justify-between'>
+                            <div className='flex flex-col'>
+                                <span className='text-md text-blue-300'>price</span>
+                                <b className='text-lg font-black text-blue-500'>$ {productItem.price} </b>
                             </div>
-                            <button onClick={() => AddProductHandler(productItem)}>
+                            <button className={`text-sm flex items-center gap-2 h-7 px-2 my-auto border rounded transition ease-in-out hover:bg-blue-500 hover:text-white ${ checkInCart(cart,productItem) ? "bg-blue-500 text-white border-0" :"border-blue-300 text-blue-400" }`} onClick={() => AddProductHandler(productItem)}>
                                 {checkInCart(cart,productItem) ? "In cart" : "Add to cart"}
+                                {checkInCart(cart,productItem) ? "" : <FiShoppingCart /> }
+                                
                             </button>
                         </div>
 
