979754d5e883966f77cb4ab782c02a6571566ba3
setykarimi <setykarimi80@gmail.com>
2022-10-29 15:37:46 +0330

    add Auth and Cart data to checkout page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,7 +1,39 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '../Providers/AuthProvider';
+import { useCart } from '../Providers/CartProvider';
+
 const CheckoutPage = () => {
-    return ( <div>
-chekout
-    </div> );
+    const auth = useAuth();
+    const { cart, total } = useCart();
+
+    if (!cart.length) {
+        return <div>
+            <Link to='/'>Go to Shopping</Link>
+        </div>
+    }
+    return (
+        <div className='container'>
+            {auth ?
+                <section>
+                    <div>
+                        <p>{auth.name}</p>
+                        <p>{auth.email}</p>
+                        <p>{auth.phoneNumber}</p>
+                    </div>
+                    {cart && cart.map(c => {
+                        return (
+                            <div>
+                                {c.name} * {c.quantity}: {c.quantity * c.offPrice}
+                            </div>
+                        )
+                    })}
+                    <hr/>
+                    <span>total: {total}</span>
+                </section>
+                :
+                <Link to='/login'>Please login!</Link>}
+        </div>
+    );
 }
- 
+
 export default CheckoutPage;
