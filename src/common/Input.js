a37ad9ff27236857e1de7074b0f970f2a0190063
setykarimi <setykarimi80@gmail.com>
2022-10-29 23:10:36 +0330

    add tailwind classes

--- src/common/Input.js
+++ src/common/Input.js
@@ -1,15 +1,17 @@
 const Input = ({ label, name, formik, type = "text" }) => {
     return (
-        <div className="form-control">
-            <label>{label}</label>
-            <input type={type}
+        <div className="flex flex-col mb-4 text-left">
+            <label className="mb-1 font-bold">{label}</label>
+            <input
+                className="bg-slate-100 p-2 w-60 sm:w-80 rounded-md outline-1 outline-blue-200"
+                type={type}
                 name={name}
                 {...formik.getFieldProps({ name })}
             />
             {formik.errors[name] && formik.touched[name] &&
-                <div className="error">
+                <span className="text-sm text-red-500 mt-1">
                     {formik.errors[name]}
-                </div>
+                </span>
             }
         </div>
     );
