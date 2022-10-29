819982036e22232b4e73228c34f1a6a60d89e543
setykarimi <setykarimi80@gmail.com>
2022-10-29 20:52:25 +0330

    add main tag in layout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,12 +1,14 @@
 import Navigation from "../components/Navigation/Navigation";
 
-const Layout = ({children}) => {
-    return ( 
+const Layout = ({ children }) => {
+    return (
         <div>
-           <Navigation />
-            {children}
+            <Navigation />
+            <main className='md:container md:mx-auto px-5 md:px-0'>
+                {children}
+            </main>
         </div>
-     );
+    );
 }
- 
+
 export default Layout;
